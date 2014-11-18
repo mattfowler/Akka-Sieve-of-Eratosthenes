a4918e5527d9f6c2fe2610629bd96c64654cf0a6
mattfowler <matt.fow@gmail.com>
2014-11-18 10:25:14 -0500

    Naieve implementation - passes first two test cases but not last of picking an arbitrary sequence of numbers to check to prime not starting at 2.

--- src/main/scala/worker/EratosthenesWorker.scala
+++ src/main/scala/worker/EratosthenesWorker.scala
@@ -4,6 +4,7 @@ import akka.actor.Actor
 import exception.InvalidInputException
 import message.{CalculatePrimesMessage, PrimesResult}
 
+import scala.collection.mutable
 
 class EratosthenesWorker extends Actor {
 
@@ -15,7 +16,19 @@ class EratosthenesWorker extends Actor {
     if(start < 1 || end < 1 || start > end) {
       throw new InvalidInputException("Start and end must be nonzero, positive and start must be less than end")
     }
-    List()
+    var numbers = mutable.ArraySeq.tabulate(end - start + 1)(i => i  + start)
+    var primes: List[Int] = List()
+
+    while(numbers.length > 0) {
+      val nextPrime:Int = numbers.head
+      numbers = numbers.drop(1)
+      for(i <- nextPrime * nextPrime to end by nextPrime ) {
+        numbers = numbers diff mutable.ArraySeq(i)
+      }
+      primes = primes :+ nextPrime
+    }
+
+    primes
   }
 
 }
