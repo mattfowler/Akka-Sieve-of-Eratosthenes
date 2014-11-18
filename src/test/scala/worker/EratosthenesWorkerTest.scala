d02ccfd96204e3c2355bf9c6af7b3bbdb66aa672
mattfowler <matt.fow@gmail.com>
2014-11-18 10:06:48 -0500

    Changing failing tests to properly use the akka test kit.

--- src/test/scala/worker/EratosthenesWorkerTest.scala
+++ src/test/scala/worker/EratosthenesWorkerTest.scala
@@ -1,20 +1,40 @@
 package worker
 
+
+import java.util.concurrent.TimeUnit
+
 import akka.actor.ActorSystem
-import akka.testkit.TestActorRef
+import akka.testkit.{TestKit, TestActorRef}
+import akka.util.Timeout
 import exception.InvalidInputException
-import message.{CalculatePrimesMessage, PrimesResult}
-import org.scalatest.{FlatSpec, Matchers}
+import message.{PrimesResult, CalculatePrimesMessage}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FlatSpecLike, Matchers}
+import akka.pattern.ask
+import scala.util.Success
 
-class EratosthenesWorkerTest extends FlatSpec with Matchers {
 
-  implicit val system = ActorSystem.create()
+@RunWith(classOf[JUnitRunner])
+class EratosthenesWorkerTest extends TestKit(ActorSystem()) with FlatSpecLike with Matchers
+{
+  implicit val timeout = Timeout(3, TimeUnit.SECONDS)
+
   val eratosthenesActor = TestActorRef[EratosthenesWorker]
 
   it should "calculate primes between two numbers" in {
-    eratosthenesActor.receive(CalculatePrimesMessage(2, 2)) should equal(PrimesResult(List(2, 2)))
-    eratosthenesActor.receive(CalculatePrimesMessage(2, 10)) should equal(PrimesResult(List(2, 3, 5, 7)))
-    eratosthenesActor.receive(CalculatePrimesMessage(2, 10)) should equal(PrimesResult(List(23, 29, 31, 37)))
+    val future = eratosthenesActor ? CalculatePrimesMessage(2, 2)
+    val Success(result: PrimesResult) = future.value.get
+    result should be(PrimesResult(List(2)))
+
+    val startOfNaturalNums = eratosthenesActor ? CalculatePrimesMessage(2, 10)
+    val Success(naturalResult: PrimesResult) = startOfNaturalNums.value.get
+    naturalResult should be(PrimesResult(List(2, 3, 5, 7)))
+
+    val middleOfNumberList = eratosthenesActor ? CalculatePrimesMessage(20, 40)
+    val Success(middleListResult: PrimesResult) = middleOfNumberList.value.get
+    middleListResult should be(PrimesResult(List(23, 29, 31, 37)))
+
   }
 
   it should "throw an error if start is not less than end or numbers are negative" in {
